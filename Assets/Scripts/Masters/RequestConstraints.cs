12fe676893c50c993d0310180114d8b896acb341
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-10-14 12:04:48 -0700

    Fuck you I'm making classes, call me the fucking school board

---
+++ Assets/Scripts/Masters/RequestConstraints.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BuildingConstraints
+{
+    public static float MaxBuildTime = 30f;
+    public static int MaxCost = 10000;
+}
+
+public class RequestWeigths
+{
+    public float time;
+    public float money;
+    public float importance;
+
+    public RequestWeigths(float time, float money, float importance)
+    {
+        this.time = time;
+        this.money = money;
+        this.importance = importance;
+    }
+}
+
+public class SchedulingParams
+{
+    int currentDay;
+    int maxDeadlineDay;
+
+    public SchedulingParams(int currentDay, int maxDeadlineDay)
+    {
+        this.currentDay = currentDay;
+        this.maxDeadlineDay = maxDeadlineDay;
+    }
+}
--- Assets/Scripts/Masters/RequestFactory.cs
+++ Assets/Scripts/Masters/RequestFactory.cs
@@ -1,39 +1,115 @@
+using Buildings;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public interface IRequest
 {
-    int GetImportance(float timeWeight, float moneyWeight, float timeToBuild, int cost);
-    int GetDeadline();
+    ImportanceFactor GetImportance(float timeWeight, float moneyWeight);
+    int GetDeadline(float timeWeight, float moneyWeight, float importanceFactor);
 
     
 
-    RelationType GetRelationType();
+    
 }
 
+public struct BuildingConstraints
 public class GenericRequest : IRequest
 {
+    public float buildTime;
     public float maxBuildTime;
 
+    int cost;
     public int maxCost;
-    public int GetDeadline()
-    {
-        throw new System.NotImplementedException();
-    }
 
-    public int GetImportance(float timeWeight, float moneyWeight, float timeToBuild, int cost)
-    {
-        float timeValue = timeWeight * (timeToBuild / maxBuildTime);
-        float costValue = moneyWeight * (cost / maxCost);
+    int currentDay;
+    public int maxDeadline;
 
-        return Mathf.CeilToInt(1 + 9 * (timeValue + costValue));
-    }
+    public ImportanceFactor importance;
+    public RelationType relationType;
 
-    public RelationType GetRelationType()
+    public int deadline;
+
+    public GenericRequest(float timeWeight, float moneyWeight, 
+        float importanceWeight, float buildTime,float maxBuildTime, int cost,
+        int maxCost, int currentDay, int maxDeadline, RelationType relationType)
     {
-        throw new System.NotImplementedException();
+        this.buildTime = buildTime;
+        this.maxBuildTime = maxBuildTime;
+        this.cost = cost;
+        this.maxCost = maxCost;
+        this.currentDay = currentDay;
+        this.maxDeadline = maxDeadline;
+        this.importance = GetImportance(timeWeight, moneyWeight);
+        this.relationType = relationType;
+        this.deadline = GetDeadline(timeWeight, moneyWeight, importanceWeight);
     }
 
     
+    public ImportanceFactor GetImportance(float timeWeight, float moneyWeight)
+    {
+        float timeValue = timeWeight * (buildTime / maxBuildTime);
+        float costValue = moneyWeight * (cost / maxCost);
+
+        int result =  Mathf.CeilToInt(1 + 9 * (timeValue + costValue));
+
+        if (result < 1)
+        {
+            return ImportanceFactor.Negligible;
+        }
+
+        else if ( result > 10)
+        {
+            return ImportanceFactor.Maximum;
+        }
+        else
+        {
+            return (ImportanceFactor)result;
+        }
+        
+    }
+
+    public int GetDeadline(float timeWeight, float moneyWeight, float importanceFactor)
+    {
+        float timeValue = timeWeight * (buildTime / maxBuildTime);
+        float costValue = moneyWeight * (cost / maxCost);
+        float importanceValue = importanceFactor * (importanceFactor / 10);
+        int deadlineValue = maxDeadline - currentDay;
+
+        int result = Mathf.CeilToInt(currentDay + (timeValue + costValue + importanceValue) * deadlineValue);
+        
+        if (result < currentDay)
+        {
+            return currentDay;
+        }
+
+        else if (result > maxDeadline)
+        {
+            return maxDeadline;
+        }
+        else
+        {
+            return result;
+        }
+    } 
+}
+
+public class KingRequest : GenericRequest
+{
+    public KingRequest(float timeWeight, float moneyWeight, float importanceWeight,
+        float buildTime, float maxBuildTime, int cost, int maxCost, int currentDay, int maxDeadline)
+        : base(timeWeight, moneyWeight, importanceWeight, buildTime,
+            maxBuildTime, cost, maxCost, currentDay, maxDeadline, RelationType.King)
+    {
+
+    }
+    
+}
+public abstract class RequestFactory
+{
+    public RequestFactory(building) 
+    {
+    }
 }
